7b5526429748542a91bc1b9c29d9cd12d06df0ea
Ben <ben247@gmail.com>
2021-04-15 14:28:03 +0100

    Had to revert because of Purge CSS v8 issue

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+    <React.Fragment>{/* React.Fragment Shorhand <></> */}
+        <h1>Hello, welcome to my blog</h1>
+    <p>
+        Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.
+    </p>
+    <p>
+        Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.
+    </p>
+    </React.Fragment>
+);
+
+export default HomePage;
