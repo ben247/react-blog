105ba5c737f08e11a90c68f041f977c129be5c4d
Ben <ben247@gmail.com>
2021-04-21 15:30:27 +0100

    Adding useEffect, useState to populate upvotes

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ArtliclesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
 import articleContent from "./article-content";
 
 const ArticlePage = ({ match }) => {
-
   const name = match.params.name;
-  const article = articleContent.find((article) => article.name === name);
+  const article = articleContent.find(article => article.name === name);
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+  }, [name]);
 
   if (!article) return <NotFoundPage />;
-  const otherArticles = articleContent.filter(
-    (article) => article.name !== name
-  );
+
+  const otherArticles = articleContent.filter((article) => article.name !== name);
 
   return (
     <React.Fragment>
       {/* React.Fragment Shorhand is <></> */}
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
